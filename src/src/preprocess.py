e532a2ae7e7c6ce0a4539adb90f28a7626ff0b3b
Dhanashri Bende <dhanashribende@gmail.com>
2026-07-03 14:54:53 +0530

    Updated spam classifier code

--- src/src/preprocess.py
+++ src/src/preprocess.py
@@ -1,23 +1,22 @@
 import pandas as pd
 
+def load_and_clean(path):
 #load the data (This defines 'df' so puthon knows what it is!)
-df=pd.read_csv("spam.csv", encoding="latin-1")
-
+    df=pd.read_csv(path, encoding="latin-1")
+ 
 #renaming column 1 and column 2 because they do not have proper names(I AM MODIFYING THE SAME DF HERE, INSTEAD OF SAVING TO NEW DATAFRAME)
-df.rename(columns={'v1': 'label', 'v2': 'message'}, inplace=True)
+    df.rename(columns={'v1': 'label', 'v2': 'message'}, inplace=True)
 
 
 #Deleting the empty columns(AGAIN MODIFYING)
-df.drop(columns=['Unnamed: 2','Unnamed: 3','Unnamed: 4'], inplace=True)
+    df.drop(columns=['Unnamed: 2','Unnamed: 3','Unnamed: 4'], errors="ignore" inplace=True)
 
-#print the first few rows to verify it worked
-print(df.head())
-
+    df=df[["label", "message"]]
 #count number of columns
-print(df["label"].value_counts())
+    # print(df["label"].value_counts())
 
 #ML preprocessing(CONVERTING SPAM AND HAM TO BINARY)
-df['label']=df['label'].map({'ham':0, 'spam':1})
+    df['label']=df['label'].map({'ham':0, 'spam':1})
 
-#print
-print(df.head())
+
+    return df
