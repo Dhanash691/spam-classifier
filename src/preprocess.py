1b125ca2112f821dc01e6e8fc199a90c8f839b54
Dhanashri Bende <dhanashribende@gmail.com>
2026-07-13 12:08:04 +0530

    i processed the text messages for handling vectorization

--- src/preprocess.py
+++ src/preprocess.py
@@ -7,9 +7,8 @@ def load_and_clean(path):
 #renaming column 1 and column 2 because they do not have proper names(I AM MODIFYING THE SAME DF HERE, INSTEAD OF SAVING TO NEW DATAFRAME)
     df.rename(columns={'v1': 'label', 'v2': 'message'}, inplace=True)
 
-
 #Deleting the empty columns(AGAIN MODIFYING)
-    df.drop(columns=['Unnamed: 2','Unnamed: 3','Unnamed: 4'], errors="ignore" inplace=True)
+    df.drop(columns=['Unnamed: 2','Unnamed: 3','Unnamed: 4'], errors="ignore", inplace=True)
 
     df=df[["label", "message"]]
 #count number of columns
@@ -18,5 +17,19 @@ def load_and_clean(path):
 #ML preprocessing(CONVERTING SPAM AND HAM TO BINARY)
     df['label']=df['label'].map({'ham':0, 'spam':1})
 
+#remove Remove rows with missing labels/messages
+    df = df.dropna(subset=["label", "message"])
+
+# Remove duplicate messages
+    df.drop_duplicates(inplace=True)
+
 
     return df
+
+if __name__ == "__main__":
+    df = load_and_clean("data/spam.csv")
+    print(df["label"].value_counts())
+    print()
+    print(df.info())
+    print()
+    print(df["label"].value_counts())
---
+++ src/text_process.py
@@ -0,0 +1,31 @@
+import re
+import string
+import nltk
+
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+nltk.download("stopwords") #downloads the english stopwords list the first time you run the program
+
+stemmer=PorterStemmer()
+stop_words=set(stopwords.words("english"))
+#we created this as reusable objects outside the function so programme can reuse them and does not have to recreate them every single time it process a message
+
+def preprocess_text(text):
+    text=text.lower() # converts text into alll lower case
+    text=text.translate(str.maketrans("", "", string.punctuation)) #removes punctuation
+    # make trans accepts three input (x,y,z) x-- charcters you want to replace, y--characters you want to replace them with, z--characters you want to delete entirely
+    words=text.split()
+
+    processed_words=[]
+    
+    for word in words:
+        if word not in stop_words:
+            processed_words.append(stemmer.stem(word)) #each word which is not a stop word(which we imported from ntlk library), is added to processed words after reducing the word to its root form using porter stemmer
+
+    return " ".join(processed_words)
+
+if __name__ == "__main__":
+    sample = "Congratulations!! You have WON a FREE lottery."
+
+    print(preprocess_text(sample))
